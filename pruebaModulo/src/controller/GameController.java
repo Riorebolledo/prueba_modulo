03946dc8b685848304d8d7d615d79ba0639b24a0
desarrollo <desarrollo@debian>
2020-06-28 11:05:57 -0400

    refactor method rangeCoordinatesValid

--- pruebaModulo/src/controller/GameController.java
+++ pruebaModulo/src/controller/GameController.java
@@ -5,10 +5,11 @@ import java.util.List;
 import Model.Carro;
 import Model.Coordinates;
 import Model.Game;
+
 public abstract class GameController {
     protected Game game;
     protected List<Carro> carros;
- 
+
     public GameController(Game game) {
         this.game = game;
         this.carros = new ArrayList<>();
@@ -17,25 +18,34 @@ public abstract class GameController {
 
     public abstract void controller();
 
-    private Boolean isValidCoordinates(Coordinates coordinates){
-        return this.game.getBoard().isValid(coordinates.getFile()
-                ,coordinates.getFile());
-    } 
+    private Boolean isValidCoordinates(Coordinates coordinates) {
+        return this.game.getBoard().isValid(coordinates.getFile(), coordinates.getFile());
+    }
 
-    protected Coordinates firstCoordinateValid(){
+    protected Coordinates firstCoordinateValid() {
         Coordinates coordinateRef = new Coordinates();
-        while(this.isValidCoordinates(coordinateRef)){
-             coordinateRef = new Coordinates();
-        } 
+        while (this.isValidCoordinates(coordinateRef)) {
+            coordinateRef = new Coordinates();
+        }
         return coordinateRef;
     }
 
-    protected List<Coordinates> rangeCoordinatesValid(int range){
+    protected List<Coordinates> rangeCoordinatesValid(int range) {
         List<Coordinates> coordinatesList = new ArrayList<Coordinates>();
         Coordinates coordinatesRef = this.firstCoordinateValid();
+        int x = coordinatesRef.getFile();
+        int y = coordinatesRef.getColumn();
+        //ordenar en tablero//
+        if (this.game.getBoard().isValid(x, (y + range))) {
             
+        } else if (this.game.getBoard().isValid((x + range), y)) {
 
-                                                            
+        } else if (this.game.getBoard().isValid((x - range), y)) {
+
+        } else if (this.game.getBoard().isValid(x, (y - range))) {
+
+        }
+
         return null;
     }
 
